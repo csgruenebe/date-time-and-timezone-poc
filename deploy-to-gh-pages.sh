58b9ac71dfd8c5ddfde811f5aead3f19457a236b
Bernhard Grünewaldt <bernhard.gruenewaldt@comsysto.com>
2018-01-30 11:28:57 +0100

    fix

--- deploy-to-gh-pages.sh
+++ deploy-to-gh-pages.sh
@@ -4,10 +4,6 @@ set -e
 
 yarn build
 mv dist ../deploy-dist
-mkdir ../bkp_deploy/
-mv .idea ../bkp_deploy/
-mv *.iml ../bkp_deploy/
-mv node_modules ../bkp_deploy/
 git fetch -p
 git checkout gh-pages
 git pull
@@ -18,6 +14,3 @@ git add . -A
 git commit -m "deploy"
 git push
 git checkout master
-mv ../bkp_deploy/* .
-mv ../bkp_deploy/.[!.]* .
-rm -rf ../bkp_deploy/
